8b257563dfdfad2d3b9a7f7c1a59d128bd87a427
pramod0608 <88960637+pramod0608@users.noreply.github.com>
2024-01-08 02:02:36 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -36,6 +36,7 @@ data "aws_subnets" "public" {
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
+  availability_zone = "us-east-1a"
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
